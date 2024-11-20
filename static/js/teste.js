7e7e3d3be2b79893597ce66af20565be632c40ca
jonatha silva <jonatha.sviegas@hotmail.com>
2024-11-20 17:33:44 -0300

    Nova vesão

---
+++ static/js/teste.js
@@ -0,0 +1,7 @@
+function Mudar_cor_botao_geral_entrou(){
+    event.target.style.backgroundColor="#F58AA6";
+}
+
+function Mudar_cor_botao_geral_saiu(){
+    event.target.style.backgroundColor="";
+}
